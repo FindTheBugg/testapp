824422a730c453f53d0057240b52187b616f1bb5
FindTheBugg <vadim2012-2013@mail.ru>
2025-03-29 15:04:46 +0300

    добавлено приложение в репозиторий

---
+++ testAppStoryboard/testAppStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testAppStoryboard
+//
+//  Created by vadim on 29.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
