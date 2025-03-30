c193bc60fa2b8a4e8e2266f158c9955c7f6e2735
FindTheBugg <vadim2012-2013@mail.ru>
2025-03-30 23:31:28 +0300

    на кнопку добавлена логика
    при тапе картинка меняется

--- testAppStoryboard/testAppStoryboard/ViewController.swift
+++ testAppStoryboard/testAppStoryboard/ViewController.swift
@@ -8,15 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var isSun: Bool = false
 
     @IBOutlet weak var skyImageView: UIImageView!
     
     @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        skyImageView.image = UIImage(systemName: "moon")
+        skyImageView.tintColor = .gray
+        changeButton.tintColor = .brown
     }
 
-
+    @IBAction func buttonDidtap(_ sender: Any) {
+        
+        if isSun {
+            skyImageView.image = UIImage(systemName: "moon")
+        } else {
+            skyImageView.image = UIImage(systemName: "sun.max")
+        }
+        isSun.toggle()
+        
+    }
+    
 }
 
